ab141e5aba55d44a56c666a9a6c00443da1ef9bd
Johnny Magrippis <j@magrippis.com>
2019-03-01 15:39:07 +0000

    Initial commit with project scaffold

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+import 'reset-css'
+
+import { App } from './App'
+
+const root = document.getElementById('root')
+
+ReactDOM.render(<App />, root)
